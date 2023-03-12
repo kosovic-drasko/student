c5dc8be4154a1fb4193792e96ff50ab09acf5999
kosovic-drasko <kosovic.drasko@montefarmco.me>
2023-03-12 08:27:59 +0100

    Deploy to Heroku

--- src/main/java/student/web/rest/PotrosnjaGorivaResource.java
+++ src/main/java/student/web/rest/PotrosnjaGorivaResource.java
@@ -1,25 +1,11 @@
 package student.web.rest;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.text.DecimalFormat;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import student.domain.PotrosnjaGoriva;
 import student.service.PotrosnjaGorivaService;
-import student.service.PotrosnjaService;
-import student.web.rest.errors.BadRequestAlertException;
-import tech.jhipster.web.util.HeaderUtil;
-import tech.jhipster.web.util.ResponseUtil;
 
 /**
  * REST controller for managing {@link student.domain.PotrosnjaGoriva}.
